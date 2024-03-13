f34162aebab4d630fe0c81e955485184ff99cc08
Aman <aman.kumar@softsensor.ai>
2024-03-13 20:00:14 +0530

    add Markdown editor

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Layout = () => (
   <div className="flex items-center justify-center h-screen min-w-screen">
-    <div className="px-2 lg:px-8 w-full h-full">
+    <div className="px-2 lg:px-8 w-full h-full min-h-0 flex flex-col">
       <Navbar />
       <Outlet />
     </div>
