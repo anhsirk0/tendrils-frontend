8ff7562be1ef94b2cbd370d221ac68c77f80ccea
krishna <krishna404@yandex.com>
2024-07-07 17:37:39 +0530

    update stuff

--- src/environment.ts
+++ src/environment.ts
@@ -4,7 +4,7 @@ type Environment = { name: EnvName; apiUrl: string };
 function getEnvironment(): Environment {
   if (window.location.href.includes("localhost"))
     return { name: "local", apiUrl: "http://localhost:8000/" };
-  return { name: "prod", apiUrl: "http://localhost:8000/" };
+  return { name: "prod", apiUrl: "https://tendrils-backend.onrender.com/" };
 }
 
 const environment = getEnvironment();
